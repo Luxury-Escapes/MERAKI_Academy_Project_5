2d3b2f2fbe70a685bb656ec1b8efd3125f816f5f
Moawiah999 <muawiyah454@gmail.com>
2024-11-11 20:39:48 +0300

    update databases

--- backend/models/Schema.sql
+++ backend/models/Schema.sql
@@ -30,7 +30,8 @@ CREATE TABLE users (
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   country VARCHAR(255),
-  role_id INT,
+  age INT,
+  role_id INT, 
   is_deleted SMALLINT DEFAULT 0,
   FOREIGN KEY (role_id) REFERENCES roles(id),
   PRIMARY KEY (user_id)
@@ -111,14 +112,13 @@ CREATE TABLE reservations (
   flight_id INT,
   hotel_id INT,
   tour_package_id INT,
-
   reservation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   status VARCHAR(50) DEFAULT 'Pending',
   is_deleted SMALLINT DEFAULT 0,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (flight_id) REFERENCES flights(id),
-  FOREIGN KEY (hotel_id) REFERENCES hotels(id),
-  FOREIGN KEY (tour_package_id) REFERENCES tour_packages(id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (flight_id) REFERENCES flights(flights_id),
+  FOREIGN KEY (hotel_id) REFERENCES hotels(hotel_id),
+  FOREIGN KEY (tour_package_id) REFERENCES tour_packages(tour_packages_id),
   PRIMARY KEY (reservation_id)
 );
 
